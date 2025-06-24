7b017be191b5ffaa0fcd22dbd849c756bffba71f
DHawked <dhawked@gmail.com>
2025-06-24 21:11:13 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Lütfen bir sayı giriniz: ");
+        int sayi = int.Parse(Console.ReadLine());
+
+        if (sayi == 10)
+            Console.WriteLine("Girilen sayı 10'a eşittir.");
+        else if (sayi < 10)
+            Console.WriteLine("Girilen sayı 10'dan küçüktür.");
+        else
+            Console.WriteLine("Girilen sayı 10'dan büyüktür.");
+
+        Console.WriteLine(sayi % 2 == 0 ? "Girilen sayı çifttir." : "Girilen sayı tektir.");
+    }
+}
