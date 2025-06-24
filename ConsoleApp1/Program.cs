704dec2256d3590fcd4005921ae5bd2b66c19693
DHawked <dhawked@gmail.com>
2025-06-24 21:12:45 +0300

    Yorum satırları eklendi.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,9 +4,11 @@ class Program
 {
     static void Main()
     {
+        // Kullanıcıdan sayı alınır.
         Console.Write("Lütfen bir sayı giriniz: ");
         int sayi = int.Parse(Console.ReadLine());
 
+        // Sayı 10'a eşit mi, küçük mü, büyük mü kontrol edilir.
         if (sayi == 10)
             Console.WriteLine("Girilen sayı 10'a eşittir.");
         else if (sayi < 10)
@@ -14,6 +16,7 @@ class Program
         else
             Console.WriteLine("Girilen sayı 10'dan büyüktür.");
 
+        // Sayı çift mi, tek mi kontrol edilir ve ekrana yazılır.
         Console.WriteLine(sayi % 2 == 0 ? "Girilen sayı çifttir." : "Girilen sayı tektir.");
     }
 }
